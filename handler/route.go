3bdfbdb39100a71ede186db1eb18067cb6c9c180
YueHonghui <hongf.yue@hotmail.com>
2019-11-28 12:01:33 +0800

    fix self version api path

--- handler/route.go
+++ handler/route.go
@@ -1,17 +1,17 @@
 package handler
 
 import (
-	"github.com/gin-gonic/gin"
 	"github.com/ctripcloud/starter/logger"
 	pkgin "github.com/ctripcloud/starter/pkg/gin"
+	"github.com/gin-gonic/gin"
 )
 
 //NewHTTPHandler create a handler to serve by http server
 func NewHTTPHandler() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Recovery(), pkgin.PromObserve("httpserver_request_count", "httpserver_request_latency"), logger.AccessLoggerForGin())
-	apis := r.Group("/apis/")
-	self := apis.Group("/_self")
+	//	apis := r.Group("/apis/")
+	self := r.Group("/_self")
 	self.GET("/version", SelfVersion)
 	self.GET("/prometheus", SelfPrometheusMetrics)
 	return r
